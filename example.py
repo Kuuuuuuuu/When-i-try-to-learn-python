a0ec45c33da0be2b6449608fdfa20d21c3829f0c
MelidaDev! <69802296+MelidaZ@users.noreply.github.com>
2021-09-17 10:58:10 +0700

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 #first Example
 print("--------------------------------")
 x = input("Enter Your Name: ")
-if(x == null) return print("Please Input Example")
 print(x)
 print("--------------------------------")
